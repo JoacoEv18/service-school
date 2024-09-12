7f39d106e3d70e2bf12aff4e7f216d1ee919338e
Joaco <joaquineliasvega@gmail.com>
2024-09-12 12:19:16 -0300

    third-commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
     <div className="grid" >
       <div className="nav">
-        <h1>AvivamientoSI</h1>
+        <h1>Avivamiento <a>SI</a></h1>
       </div>
         <section className="sectionTitulo">
           <h1 className="titulo">Escuela de Servicio</h1>
@@ -24,17 +24,18 @@ function App() {
           </div>
           <div className="player">
             <ReactPlayer
+            className="react-player"
             url={URL}
             playing={false}
             volume={0}
-            height={480}
-            width={850}
+            width='80%'
+            height='85%'
             />
           </div>
         </section>
         <section className="sectionQuestion">
           <div className="insc">
-            <h1 >Inscripciones 2024</h1>
+            <h1 >Inscripciones <a>2024</a></h1>
           </div>
           <div className="form">
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfC7y9Hzx4Pi9GRREbJgvdvUbr-wiRDfe8Jioap5VSEiChtZg/viewform?embedded=true" width="640" height="640" frameBorder="0" marginHeight="0" marginWidth="0">Cargando…</iframe>
